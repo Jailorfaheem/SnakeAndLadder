1d58fb14b41a906d41b47d32feb18b1d7c6bbc3a
faheemjailor <faheemjailor0055>
2021-11-24 17:40:32 +0530

    faheem UC1 for single player

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**Welcome to the game of snake and ladder**");
+            Console.WriteLine();
+            Console.WriteLine("Single Player mode: Starting position 0");  //as per UC1
+            Console.WriteLine("....Game Begins....");
+            int position = 0;
+            int dice;
+
+        }
+    }
+}
